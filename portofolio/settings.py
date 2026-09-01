1379afa7f39e0dd0902843d6b435373f8c90b9e3
adinatapranaja <adinatapranaja@users.noreply.github.com>
2026-09-01 12:43:09 +0700

    preparing views, url, templates, and static files

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'portofolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,6 +72,8 @@ TEMPLATES = [
     },
 ]
 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 WSGI_APPLICATION = 'portofolio.wsgi.application'
 ASGI_APPLICATION = 'portofolio.asgi.application'
 
