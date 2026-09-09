ccd9bc18734717ba17801468559d1259e85e7200
adinatapranaja <adinatapranaja@users.noreply.github.com>
2026-09-09 19:25:06 +0700

    implement views and dynamic templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Views for the main application are added in a later tutorial step.
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        'name': 'Adinata Alaudin Pranaja',
+        'short_name': 'Adinata',
+        'npm': '2506656356',
+        'study_program': 'S1 Sistem Informasi',
+        'bio': (
+            'Full Stack Developer | Purwadhika Graduate | '
+            'University of Indonesia Information System Student'
+        ),
+    }
+    return render(request, 'index.html', context)
+
+
+def show_experience(request):
+    context = {
+        'name': 'Adinata Alaudin Pranaja',
+        'short_name': 'Adinata',
+        'experience_list': Experience.objects.all(),
+    }
+    return render(request, 'experience.html', context)
