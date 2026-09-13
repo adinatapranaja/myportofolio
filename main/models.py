2634ca963b03a52553e763cf00bc774ea1a6b27d
adinatapranaja <adinatapranaja@users.noreply.github.com>
2026-09-13 21:11:32 +0700

    add project model and migrations

--- main/models.py
+++ main/models.py
@@ -31,3 +31,15 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    technologies = models.CharField(max_length=255)
+    project_url = models.URLField(blank=True)
+    repository_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
